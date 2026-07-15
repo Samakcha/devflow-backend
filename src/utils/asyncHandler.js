c9f4da6d78f936533315e0482f972756f4839a09
Samakcha Kumar Mishra <samakchakrmishra03@gmail.com>
2026-07-15 22:29:47 +0530

    refactor: remove unused asyncHandler for Express 5

--- src/utils/asyncHandler.js
+++
@@ -1,7 +0,0 @@
-const asyncHandler = (controller) => {
-    return (req, res, next) => {
-        Promise.resolve(controller(req, res, next)).catch(next);
-    };
-};
-
-export default asyncHandler;
